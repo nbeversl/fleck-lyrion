8333df24b937b69658f9d059afe2bd46bdd84343
Nate Beversluis <nathanielbeversluis@gmail.com>
2023-09-07 08:43:28 -0400

    reinsitute corsanywhere to make Yamaha volume changes work relative to current value

--- src/Yamaha/Yamaha.jsx
+++ src/Yamaha/Yamaha.jsx
@@ -13,17 +13,39 @@ class Yamaha {
     this.lastVolumeChange = new Date().getTime();
 
     this.APICall = (body, callback) => {
-      fetch(this.ip + "/YamahaRemoteControl/ctrl", {
-        method: "POST",
-        headers: { "Content-Type": "text/plain" },
-        body: body,
-      });
+      fetch(
+        "http://10.0.0.71:8080/http://10.0.0.68:80/YamahaRemoteControl/ctrl",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "text/plain",
+          },
+          body: body,
+        }
+      )
+        .then((response) => response.text())
+        .then((data) => {
+          console.log(data);
+          if (callback) {
+            callback(data);
+          }
+        });
     };
-    this.volume = -300;
 
     this.setMediaCenter = () => {
       this.APICall(
         '<YAMAHA_AV cmd="PUT"><Main_Zone><Input><Input_Sel>AV5</Input_Sel></Input></Main_Zone></YAMAHA_AV>'
+      );
+    };
+
+    this.getVolume = () => {
+      return this.APICall(
+        '<YAMAHA_AV cmd="GET"><Main_Zone><Volume><Lvl>GetParam</Lvl></Volume></Main_Zone></YAMAHA_AV>',
+        (text) => {
+          text = text.replace(/<[^>]*>/g, "");
+          text = text.replace("1dB", "");
+          return text;
+        }
       );
     };
 
@@ -34,22 +56,34 @@ class Yamaha {
     };
 
     this.volumeUp = () => {
-      var new_volume = this.volume + 5;
-      this.volume += 5;
       this.APICall(
-        '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>' +
-          new_volume.toString() +
-          "</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>"
+        '<YAMAHA_AV cmd="GET"><Main_Zone><Volume><Lvl>GetParam</Lvl></Volume></Main_Zone></YAMAHA_AV>',
+        (text) => {
+          text = text.replace(/<[^>]*>/g, "");
+          text = text.replace("1dB", "");
+          var newVolume = parseInt(text) + 5;
+          this.APICall(
+            '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>' +
+              newVolume.toString() +
+              "</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>"
+          );
+        }
       );
     };
 
     this.volumeDown = () => {
-      var new_volume = this.volume - 5;
-      this.volume -= 5;
       this.APICall(
-        '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>' +
-          new_volume.toString() +
-          "</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>"
+        '<YAMAHA_AV cmd="GET"><Main_Zone><Volume><Lvl>GetParam</Lvl></Volume></Main_Zone></YAMAHA_AV>',
+        (text) => {
+          text = text.replace(/<[^>]*>/g, "");
+          text = text.replace("1dB", "");
+          var newVolume = parseInt(text) - 5;
+          this.APICall(
+            '<YAMAHA_AV cmd="PUT"><Main_Zone><Volume><Lvl><Val>' +
+              newVolume.toString() +
+              "</Val><Exp>1</Exp><Unit>dB</Unit></Lvl></Volume></Main_Zone></YAMAHA_AV>"
+          );
+        }
       );
     };
 
