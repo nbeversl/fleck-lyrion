b9bf8927cf1703b9383a4f335639541459bfb22d
Nate Beversluis <nathanielbeversluis@gmail.com>
2024-07-11 08:15:56 -0400

    next/previous track now working

--- src/BrowserPlayer.jsx
+++ src/BrowserPlayer.jsx
@@ -11,52 +11,54 @@ class BrowserPlayer {
     this.audio = null;
 
     this.getPlayerStatus = (callback) => {
-        callback({
-          time: this.audio ? this.audio.currentTime : 0,
-          duration: this.audio ? this.audio.duration : 0,
-          playlist_loop : this.tracks,
-          playlist_cur_index : this.currentIndex
-        })
-      },
+      callback({
+        time: this.audio ? this.audio.currentTime : 0,
+        duration: this.audio ? this.audio.duration : 0,
+        playlist_loop : this.tracks,
+        playlist_cur_index : this.currentIndex
+      })
+    },
 
     this.seek = (time) => {
-        this.currentTime = time;
-      }
+      this.currentTime = time;
+    }
 
     this.playAlbumFromTrackAndContinue = (track, startNumber) => {
-        var albumID = track.album_id;
-        this.LMS.request(
+      var albumID = track.album_id;
+      this.LMS.request(
+        [
+          "",
           [
-            "",
-            [
-              "titles",
-              "0",
-              "100",
-              "album_id:" + albumID,
-              "sort:tracknum",
-              "tags:**e**",
-            ],
+            "titles",
+            "0",
+            "100",
+            "album_id:" + albumID,
+            "sort:tracknum",
+            "tags:**e**",
           ],
-          (r) => {
-            this.currentIndex = parseInt(startNumber);
-            this.tracks = r.result.titles_loop;
-            this.playing = true;
-            console.log(this.tracks[this.currentIndex])
-            this.audio = new Audio(this.LMS.getTrack(this.tracks[this.currentIndex].id.toString()))
-            this.audio.load()
-            this.audio.play()
-        });
+        ],
+        (r) => {
+          this.currentIndex = parseInt(startNumber);
+          this.tracks = r.result.titles_loop;
+          this.playing = true;
+          this.playCurrentTrack()
+      });
+    }
+
+    this.playCurrentTrack = () => {
+      if (this.audio) {
+        this.audio.pause();
+      }
+      this.currentTrack = this.LMS.getTrack(this.tracks[this.currentIndex].id.toString())     
+      this.audio = new Audio(this.currentTrack)
+      this.audio.load()
+      this.audio.play()      
     }
 
     this.playOrPause = () => {
-      console.log('CLICKED')
-      console.log(this.audio)
-
       if (this.playing) {
-        console.log('PAUSING')
         this.audio.pause(); // Pause the audio
       } else {
-        console.log('PLAYGIN')
         this.audio.play(); // Play the audio
       }
       this.playing = ! this.playing
@@ -69,45 +71,16 @@ class BrowserPlayer {
     this.nextTrack =  () => {
       if (this.currentIndex < this.tracks.length - 1) {
         this.currentIndex += 1;
+        this.playCurrentTrack()
       }
     }
 
     this.previousTrack = () => {
       if (this.currentIndex > 0) {
         this.currentIndex -=1;
+        this.playCurrentTrack()
       }
     }
-
-    // useEffect(() => {
-    //   if (!isLoading && tracks.length > 0 && currentIndex >= 0 && currentIndex < tracks.length) {
-    //     setSourceTrack(LMS.getTrack(tracks[currentIndex].id.toString()))
-    //     setAudioLoaded(false)
-    //   }
-    // }, [isLoading, tracks, currentIndex, LMS]); // Dependencies include isLoading, tracks, currentIndex, LMS
-
-    // useEffect(() => {
-    //   if (src) {
-    //     setAudio(new Audio(src))
-    //   }
-    // }, [src]);
-
-    // useEffect(() => {
-    //   if (audio && src) {
-    //     audio.load();
-    //     setAudioLoaded(true);
-    //   }
-    // }, [audio, src])
-
-    // useEffect(() => {
-    //   if (audio && audioLoaded && playing) {
-    //       audio.play();
-    //   }
-    // }, [audio, playing, audioLoaded])
-
-    // useEffect(() => {
-    //   console.log("playing changed:", playing);
-    // }, [playing]);
-
 
     this.enableNoSleep = () => {
       document.removeEventListener("click", enableNoSleep, false);
