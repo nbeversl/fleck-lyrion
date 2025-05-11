a09d51467fb0c86b60d8fe332bfdc53c35f68405
Nate Beversluis <nathanielbeversluis@gmail.com>
2025-05-11 02:03:06 +0200

    restore download SVG

--- src/svg/Download.jsx
+++ src/svg/Download.jsx
@@ -1,11 +1,10 @@
-export default function Backward({ className, ...props }) {
+export default function Download({ className, ...props }) {
   return (
     <>
     <svg className={className} viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M15.96 10.8301H12.9L12.14 13.1201H14.43C15.27 13.1201 15.96 13.8001 15.96 14.6501C15.96 15.4901 15.28 16.1801 14.43 16.1801H12.14" strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M9.54004 16.17V10.8301L8.04004 12.5001"  strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M10.02 4.46997L12 2"  strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
-      <path d="M4.90997 7.79999C3.79997 9.27999 3.10999 11.11 3.10999 13.11C3.10999 18.02 7.09 22 12 22C16.91 22 20.89 18.02 20.89 13.11C20.89 8.19999 16.91 4.21997 12 4.21997C11.32 4.21997 10.66 4.31002 10.02 4.46002"  strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round"/>
+      <g id="Interface / Download">
+        <path id="Vector" d="M6 21H18M12 3V17M12 17L17 12M12 17L7 12" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+      </g>
     </svg>
     </>
   );
